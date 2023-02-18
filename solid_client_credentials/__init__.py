5dce18001a3d8d1683204812e1eb8c5ab2d16bf3
A_A <21040751+Otto-AA@users.noreply.github.com>
2023-02-18 14:03:28 +0100

    refactor: remove tkinter example

--- solid_client_credentials/__init__.py
+++ solid_client_credentials/__init__.py
@@ -1,9 +1,9 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
-    __version__ = version('SolidClientCredentials')
+    __version__ = version("SolidClientCredentials")
 except PackageNotFoundError:
-    __version__ = '(local)'
+    __version__ = "(local)"
 
 del PackageNotFoundError
 del version
--- solid_client_credentials/__main__.py
+++ solid_client_credentials/__main__.py
@@ -5,6 +5,5 @@
 
 from solid_client_credentials.cli import main
 
-
-if __name__ == '__main__':  # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     main()  # pylint: disable=no-value-for-parameter
